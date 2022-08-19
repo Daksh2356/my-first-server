ef9098e1e164f68321cb28f1e4c5f7c5e399ece5
Daksh2356 <dakshmakhija@gmail.com>
2022-08-19 21:25:33 +0530

    Third partry dependency added (nodemon)

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const http = require("http");
 
-const port = 8081;
+const port = 8081; // Local port number
 
 http
     .createServer((request, response) => {
+        //callback function
         response.writeHead(200, { "Content-Type": "text/html" });
         response.write("<h1> Hello, this is my first server </h1>");
+        response.write(
+            "<p><b> I understood the concept of npm today and installed nodemon package</b></p>"
+        );
         response.end();
     })
     .listen(port, () => {
+        //callback function
         console.log(`Node.js started running on port ${port}`);
     });
