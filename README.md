af041485b1ac609779463c8bfa1cf21cf84e1e1e
Daksh2356 <dakshmakhija@gmail.com>
2022-08-21 02:51:14 +0530

    Learned use of GET and POST method

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-first-server
 Practicing Node Js 
+Made use of GET and POST ( server HTTP request methods )
+Made a simple TODO SERVER
--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const http = require("http");
 
-const port = 8081; // Local port number
+const port = 8081;
+
+const todoList = ["Complete Node Byte", "Play songs!!"];
 
 http
-    .createServer((request, response) => {
-        //callback function
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write("<h1> Hello, this is my first server </h1>");
-        response.write(
-            "<p><b> I understood the concept of npm today and installed nodemon package</b></p>"
-        );
-        response.end();
+    .createServer((req, res) => {
+        const { method, url } = req;
+        console.log(method, url);
+        if (url === "/todos") {
+            if (method === "GET") {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.write(todoList.toString());
+            } else if (method == "POST") {
+                let body = "";
+                req.on('error', (err) => {
+                    console.error(err);
+                }).on('data', (chunk) => {
+                    console.log(chunk);
+                    body += chunk;
+                }).on('end', () => {
+                    body = JSON.parse(body);
+                    console.log("data:", body);
+                })
+            } else {
+                res.writeHead(501);
+            }
+        } else {
+            res.writeHead(404);
+        }
+        res.end();
     })
     .listen(port, () => {
-        //callback function
         console.log(`Node.js started running on port ${port}`);
     });
