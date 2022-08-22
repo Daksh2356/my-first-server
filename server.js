1bcca78eb59693aefff462bb2a34ceaf1ba33471
Daksh2356 <dakshmakhija@gmail.com>
2022-08-23 00:55:38 +0530

    Modified the POST and DELETE request part

--- server.js
+++ server.js
@@ -5,65 +5,57 @@ const port = 8081;
 const todoList = ["Complete Node Byte", "Play games"];
 
 http
-    .createServer((req, res) => {
-        const { method, url } = req;
-        console.log(method, url);
-        if (url === "/todos") {
-            if (method === "GET") {
-                res.writeHead(200, { "Content-type": "text/html" });
-                res.write(todoList.toString());
-            } else if (method == "POST") {
-                let body = "";
-                req
-                    .on("error", (err) => {
-                        console.error(err);
-                    })
-                    .on("data", (chunk) => {
-                        console.log(chunk);
-                        body += chunk;
-                    })
-                    .on("end", () => {
-                        body = JSON.parse(body);
-                        let newArray = todoList;
-                        newArray.push(body.item);
-                        console.log(newArray);
-                        console.log(todoList);
-                        res.writeHead(201);
-                    });
-            } else if (method == "DELETE") {
-                let body = "";
-                req
-                    .on("error", (err) => {
-                        console.error(err);
-                    })
-                    .on("data", (chunk) => {
-                        body += chunk;
-                    })
-                    .on("end", () => {
-                        body = JSON.parse(body);
-                        let deleteThis = body.item;
-
-                        // for (let i = 0; i < todoList.length; i++) {
-                        //     if (todoList[i] == deleteThis) {
-                        //         todoList.splice(i, 1);
-                        //         break;
-                        //     }
-                        // }
-
-                        todoList.find((element, index) => {
-                            if (element == deleteThis) {
-                                todoList.splice(index, 1);
-                            }
-                        })
-                    });
-            } else {
-                res.writeHead(501);
-            }
-        } else {
-            res.writeHead(404);
-        }
-        res.end();
-    })
-    .listen(port, () => {
-        console.log(`Node.js started running on port ${port}`);
-    });
+  .createServer((req, res) => {
+    const { method, url } = req;
+    console.log(method, url);
+    if (url === "/todos") {
+      if (method === "GET") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(todoList.toString());
+      } else if (method == "POST") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            // console.log(chunk); --> returns a hexadecimal number
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let newArray = todoList;
+            newArray.push(body.item);
+            console.log(newArray);
+          });
+        res.writeHead(201);
+      } else if (method == "DELETE") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let deleteThis = body.item;
+            todoList.find((element, index) => {
+              if (element == deleteThis) {
+                todoList.splice(index, 1);
+              }
+            });
+          });
+        res.writeHead(204);
+      } else {
+        res.writeHead(501);
+      }
+    } else {
+      res.writeHead(404);
+    }
+    res.end();
+  })
+  .listen(port, () => {
+    console.log(`Node.js started running on port ${port}`);
+  });
